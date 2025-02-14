20d097cb0e4ebeb1d13c7c74d56b2554897e4b75
nwassom <nwassom@outlook.com>
2025-02-14 17:07:39 -0500

    EOD push

--- VO2 Max Trainer/TimerButton.swift
+++ VO2 Max Trainer/TimerButton.swift
@@ -7,11 +7,18 @@
 
 import SwiftUI
 
+/*
+    - Implement smoother transitions on notches
+    - Color change depending on rest vs run
+    - Background app refresh so that if phone locked still updates
+    - Live updates
+ 
+    - Siri / Voice integration for transitions
+ */
 struct TimerButton : View
 {
     @StateObject private var viewModel = WorkoutVM(workout: Scandanavian_4x4())
-    
-    let notchCount = 60
+    @State private var displayProgress: Double = 0.0;
     
     var body: some View
     {
@@ -22,18 +29,18 @@ struct TimerButton : View
                 i in
                 
                 let angle = Angle(degrees: (Double(i) / Double(notchCount)) * 360.0)
-                let filled = Double(i) / Double(notchCount) <= progress
+                let filled = Double(i) / Double(notchCount) <= displayProgress
                 
                 Rectangle()
                     .frame(width: 4, height: 15)
-                    .offset(y: -90)
+                    .offset(y: -135)
                     .rotationEffect(angle)
                     .foregroundColor(filled ? .blue : .blue.opacity(0.3))
             }
             
             Circle()
                 .fill(viewModel.isRunning ? Color.blue.opacity(0.2) : Color.green.opacity(0.2))
-                .frame(width: 200, height: 200)
+                .frame(width: 300, height: 300)
                 .overlay(
                     Text(viewModel.isRunning ? "\(formattedTime(viewModel.timeRemaining))" : "Start")
                         .font(.largeTitle)
@@ -47,9 +54,34 @@ struct TimerButton : View
                         viewModel.startWorkout()
                     }
                 }
+        }
+        .onChange(of: viewModel.timeRemaining, initial: true)
+        {
+            _,_ in
+            animateProgress()
         }
     }
     
+    private var notchCount: Int
+    {
+        guard let duration = viewModel.currentInterval?.duration else { return 60 }
+        
+        let minNotches = 60
+        let maxNotches = 120
+        let scaleFactor = 2.0
+        
+        let scaledNotches = minNotches + Int(Double(duration) / scaleFactor)
+        
+        return min(max(scaledNotches, minNotches), maxNotches)
+    }
+    
+    private func animateProgress()
+    {
+        withAnimation(.linear(duration: 0.1))
+        {
+            displayProgress = progress
+        }
+    }
     private var progress: Double
     {
         guard let currentInterval = viewModel.currentInterval else { return 0.0 }
